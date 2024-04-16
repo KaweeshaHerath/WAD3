b563b08e0464d9899530bd785ae63d352286abc1
Sampath G.S.N <144052515+KaweeshaHerath@users.noreply.github.com>
2024-04-16 10:10:07 +0530

    update

--- src/lib/content/featured-projects.ts
+++ src/lib/content/featured-projects.ts
@@ -11,7 +11,7 @@ const featuredProjectsSection: FeaturedProjectsSectionType = {
       tasks:
         'Developed a user-friendly and interactive mobile application that recommends movies to users based on their preferences and ratings.',
       url: 'https://drive.google.com/file/d/1SJbV7uheI36G_LOMxMc8DFYqdAL6XzgW/view?usp=drive_link', // Access to the documentation 
-      img: '/05.png',
+      img: './05.png',
       //video : '/Mobile App.mp4', ---> Can access to the vedio through the image.
       tags: ['NextJs', 
       'TypeScript', 
