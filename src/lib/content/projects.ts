17b90e2c63fe18eae136e23940a73884233a6461
Sampath G.S.N <144052515+KaweeshaHerath@users.noreply.github.com>
2024-04-16 10:17:11 +0530

    update

--- src/lib/content/projects.ts
+++ src/lib/content/projects.ts
@@ -11,7 +11,7 @@ export const projectsSection: ProjectsSectionType = {
       name: 'Calculator ',      
       url: ' Crafting Calculative Wonders: A Journey with NetBeans and Java.',
       repo: 'https://drive.google.com/drive/folders/15p0w6-8OWmRqEqJ7BD0-_k7QixiJ9ATY?usp=drive_link',
-      img: '/02.png',
+      img: './02.png',
       year: 2022,
       tags: ['Open AI', 'OOP', 'Java', 'CSS' , 'HTML' ],
     },
@@ -21,7 +21,7 @@ export const projectsSection: ProjectsSectionType = {
         name: 'Simple Lock Screen ',
         url: '/',
         repo: 'https://github.com/FHSS-USJ/assignment-02-KaweeshaHerath.git/', // Repositary URL
-        img: '/03.png',
+        img: './03.png',
         year: 2022,
         tags: ['HTML', 'CSS', 'TS'],
     },
